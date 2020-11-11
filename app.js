0b8f3937f556f5dc1414addd1769129eeb81a558
rigel selmani <rigelrigelselmani@hotmail.com>
2020-11-11 01:47:19 -0500

    creating the connection with mongodb, retreaving the data from the new form in the new file and and sending it in the database

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const express =require("express");
-const app=express();
+const express    =require("express"),
+      bodyParser = require('body-parser'),
+      mongoose   = require("mongoose"),
+      app        =express();
 
+mongoose.connect('mongodb://localhost:27017/nail', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,useFindAndModify:false}).then(res=>{
+    console.log("DB Connected!")
+}).catch(err => {
+console.log(Error, err.message);
+})
+
+var nailSchema = new mongoose.Schema({
+    name:String,
+    image:String,
+    description:String,
+})
+const Nail=mongoose.model('Nail', nailSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
@@ -9,6 +25,21 @@ app.get("/",function(req,res){
 
 app.get("/collections",function(req,res){
     res.render("Collections")
+})
+
+app.post("/collections",function(req,res){
+    const name=req.body.name;
+    const image=req.body.image;
+    const description=req.body.description;
+
+    const newNail={name:name,image:image,description:description}
+    Nail.create(newNail,function(err,newNail){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect("/collections");
+        }
+    })
 })
 
 app.get("/about",function(req,res){
