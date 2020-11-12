8a89403d6c7bab566b83301f2ad356df0c1908a4
rigel selmani <rigelrigelselmani@hotmail.com>
2020-11-11 23:20:12 -0500

    edit file and update route

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express    =require("express"),
-      bodyParser = require('body-parser'),
-      mongoose   = require("mongoose"),
-      request    = require('request'),
-      app        =express();
+const express       =require("express"),
+      bodyParser    = require('body-parser'),
+      mongoose      = require("mongoose"),
+      request       = require('request'),
+      methodOverride=require("method-override"),
+      app           =express();
 
 mongoose.connect('mongodb://localhost:27017/nail', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,useFindAndModify:false}).then(res=>{
     console.log("DB Connected!")
@@ -19,11 +20,62 @@ const Nail=mongoose.model('Nail', nailSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
+app.use(methodOverride("_method"))
 
 app.get("/",function(req,res){
     res.render("Home")
 });
 
+app.get("/collections",function(req,res){
+    Nail.find({},function(err,allNails){
+        if(err){
+            console.log(err)
+        }else{
+            res.render("Collections",{nails:allNails})
+        }
+    })
+})
+
+app.post("/collections",function(req,res){
+    const name=req.body.name;
+    const image=req.body.image;
+    const description=req.body.description;
+
+    const newNail={name:name,image:image,description:description}
+    Nail.create(newNail,function(err,newNail){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect("/collections");
+        }
+    })
+})
+//New route 
+app.get("/new",function(req,res){
+    res.render("new")
+})
+
+
+//Edit nail route
+app.get("/edit/:id",function(req,res){
+    Nail.findById(req.params.id,function(err,foundNail){
+        res.render("edit",{nail:foundNail})
+    })
+})
+//Update nail route
+app.put("/:id",function (req,res){
+    Nail.findByIdAndUpdate(req.params.id,req.body.nail,function(err,updateNail){
+        if(err){
+            console.log(err)
+            res.redirect("/")
+        }else{
+            console.log(updateNail)
+            res.redirect("/edit/"+req.params.id)
+        }
+    })
+})
+
+//Subscribtion logic get the users email.
 app.post("/",function (req,res){
     var email=req.body.subscribe
     var data={
@@ -59,37 +111,6 @@ app.post("/",function (req,res){
        });
 })
 
-app.get("/collections",function(req,res){
-    Nail.find({},function(err,allNails){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("Collections",{nails:allNails})
-        }
-    })
-})
-app.post("/collections",function(req,res){
-    const name=req.body.name;
-    const image=req.body.image;
-    const description=req.body.description;
-
-    const newNail={name:name,image:image,description:description}
-    Nail.create(newNail,function(err,newNail){
-        if(err){
-            console.log(err)
-        }else{
-            res.redirect("/collections");
-        }
-    })
-})
-
-app.get("/about",function(req,res){
-    res.render("about")
-})
-
-app.get("/new",function(req,res){
-    res.render("new")
-})
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost 3000`)
   })
